3aaf1ffaf69b140ecad96a719bf0b7af35196acf
Jorge Pérez <77463563+per18364@users.noreply.github.com>
2022-03-14 15:13:07 -0600

    mas cambios

---
+++ src/index.js
@@ -0,0 +1,13 @@
+let contenedor = document.getElementById('body-container');
+
+let lista = document.createElement('ul');
+let opcion = document.createElement('li');
+opcion.innerHTML = 'Pagina1';
+let opcion2 = document.createElement('li');
+opcion.innerHTML = 'Pagina2';
+let opcion3 = document.createElement('li');
+opcion.innerHTML = 'Pagina3';
+
+lista.appendChild(opcion);
+lista.appendChild(opcion2);
+lista.appendChild(opcion3);
